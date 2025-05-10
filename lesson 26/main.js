cb57de528940efb8e7625965ac100a85c47ae95e
duaspahiu <duaspahiu5@gmail.com>
2025-05-10 14:53:59 +0200

    lesson 26

---
+++ lesson 26/main.js
@@ -0,0 +1,76 @@
+console.log ("hello there")
+
+var x=5;
+
+console.log(x)
+
+var y=100;
+
+console.log(x+y);
+
+var a = 20
+var b = 30
+var c= a + b
+
+console.log(c)
+
+var r=60
+var z=2
+var c=60/2
+
+console.log(m)
+
+
+console.log(k)
+
+n=3
+s=2
+rezultati=n%s
+
+console.log(rezultati)
+
+console.log(12%3)
+
+Numri=70
+
+console.log(Number)
+
+numridyte=100;
+
+
+numridyte++;
+
+console.log(numridyte)
+
+x--;
+x++;
+
+console.log(x)
+
+
+console.log(".....................")
+var x=20;
+x+=10
+//x= y-10
+console.log(y)
+
+
+console.log("...................")
+var k = 20;
+k*=2
+//k*k*20
+console.log(k)
+
+
+console.log("........pjesa dyte.......")
+
+var x = 5;
+var y = 5;
+
+console.log(x==y)
+
+var e = 3;
+var f = 5;
+
+console.log(e==f)
+
