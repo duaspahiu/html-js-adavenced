2817891eb0718e45cc4d0a3fbaab86b1ce7ae604
duaspahiu <duaspahiu5@gmail.com>
2025-05-17 14:49:28 +0200

    lesson 27

---
+++ Lesson 27/js/main.js
@@ -0,0 +1,37 @@
+function showmessage(){
+  alert("this message has function")
+}
+function showmessageTwo(){
+    console.log("hello there")
+}
+showmessageTwo()
+showmessageTwo()
+
+
+
+
+function mbledhje(number1,number2){
+    return number1 + number2
+}
+
+console.log(mbledhje(100,3))
+
+function tocelcius(f){
+    return (5/9)* (f-32)
+}
+
+console.log("60 fahrenheit is equal to "+ tocelcius(60)+"celsius")
+
+ function tocelcius(minutes){
+    return (60)
+ }
+ console.log("shanderimi i minutave ne sekonda"+(5))
+  
+
+ function trianglearea(baselengthe,height){
+    return 0.5 *baselengthe*height
+ }
+
+ console.log(trianglearea(5,7))
+
+
