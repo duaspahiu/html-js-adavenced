d439008d4ac642c2df6881528eb422735b69ba74
duaspahiu <duaspahiu5@gmail.com>
2025-05-31 14:25:14 +0200

    lesson 29

---
+++ lesson 29/main.js
@@ -0,0 +1,32 @@
+var programinglangugue=['java Script','java', 'python'];
+
+console.log(programinglangugue);
+console.log(programinglangugue[2]);
+
+console.log("kemi shtuar mje elemnet ne fillim listes")
+programinglangugue.push['php']
+console.log(programinglangugue);
+
+
+console.log("kemi shtuar nje elemnet ne fillim listes")
+programinglangugue.pop()
+console.log(programinglangugue);
+
+
+console.log("kemi shtuar nje elemnet ne fillim listes")
+programinglangugue.unshift('c#')
+console.log(programinglangugue);
+
+
+console.log("kemi shtuar nje elemnet ne fillim listes")
+programinglangugue.shift('c#')
+console.log(programinglangugue);
+
+programinglangugue.splice(0.2,'ruby')
+
+console.log(programinglangugue);
+
+
+console.log(Math.random()*5)
+
+console.log(Math.floor(Math.random()))
