ffde75af0ae70b018ac6baeb24a176eb5e852f1d
duaspahiu <duaspahiu5@gmail.com>
2025-06-14 13:48:19 +0200

    lesson 31

---
+++ lesson 31/js/main.js
@@ -0,0 +1,46 @@
+var frutat = ['molla','dardha','qershia','hello']
+
+
+for(var i=0; i<frutat.length; i++){
+    console.log(frutat[i])
+}
+
+console.log("--------------------")
+
+var e = 0;
+
+do{
+    e = e +1;
+
+    console.log(e);
+
+}while (e<5)
+
+    console.log("----------------")
+
+    var j=0;
+    while (j<5){
+        console.log(j)
+        j=j+1;
+
+
+    }
+    var person = {firstname:"john", lastname: "doe",age:25}
+
+
+    for(z in person){
+        
+        console.log(person[z])
+    }
+    
+     for(z in frutat){
+        
+        console.log(frutat[z])
+    }
+
+    var teksti= "javascript";
+
+    for(l of teksti){
+        console.log(l)
+    }
+    
